8869de6594fdb9471c3bff9717b66dc5b652d05e
LuisKlopp <fbgus333@gmail.com>
2024-03-25 22:06:54 +0900

    Fix: 로고 다크모드 첫진입시 렌더링 안되는 현상 해결

--- components/navigation/navigation-logo.tsx
+++ components/navigation/navigation-logo.tsx
@@ -1,23 +1,23 @@
 "use client";
 
 import Image from "next/image";
-import { useTheme } from "next-themes";
+// import { useTheme } from "next-themes";
 
 import BlackModeLogo from "@/public/black-mode-logo.svg";
 import WhiteModeLogo from "@/public/white-mode-logo.svg";
 
 const NavigationLogo = () => {
-  const { theme } = useTheme();
-  const isDarkTheme = theme === "dark";
   return (
     <button className="min-w-[50px]">
       <Image
-        src={
-          isDarkTheme
-            ? BlackModeLogo
-            : WhiteModeLogo
-        }
+        src={BlackModeLogo}
         alt="logo"
+        className="hidden dark:block"
+      />
+      <Image
+        src={WhiteModeLogo}
+        alt="logo"
+        className="dark:hidden"
       />
     </button>
   );
