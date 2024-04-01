5d35bf763cfe76da0f300ec2c864e243f43c9470
LuisKlopp <fbgus333@gmail.com>
2024-04-01 18:32:44 +0900

    Feat: tag-box 컴포넌트 생성

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,6 +6,9 @@ const HomePage = () => {
     <div>
       <ProfileBanner />
       <div className="lg:mx-[10px] lgx:mx-[100px]">
+        <h1 className="mb-10 mt-[60px] pl-[calc((100%-400px)/2)] text-xl font-medium md:pl-[calc((100%-820px)/2)] lg:pl-[calc((100%-1240px)/2)]">
+          Most Viewed
+        </h1>
         <PostList />
       </div>
     </div>
---
+++ components/tag-box.tsx
@@ -0,0 +1,9 @@
+export const TagBox = () => {
+  return (
+    <div className="rounded-lg border border-gray05 px-4 py-1 tracking-normal dark:border-gray03">
+      <span className="text-xs font-light text-gray05 dark:text-gray03">
+        Mark up
+      </span>
+    </div>
+  );
+};
