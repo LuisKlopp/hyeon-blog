7108ac2fddffbc6c2db50f1b39c8f82e6a642a42
LuisKlopp <fbgus333@gmail.com>
2024-04-09 11:54:35 +0900

    Feat: comment 기능 퍼블리싱

--- components/comments/comment-input.tsx
+++ components/comments/comment-input.tsx
@@ -1,7 +1,7 @@
 export const CommentInput = () => {
   return (
     <input
-      className="input-custom"
+      className="input-custom dark:bg-gray07 dark:border-gray06 dark:placeholder:text-gray05"
       placeholder="이름"
     />
   );
