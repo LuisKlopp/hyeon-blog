0d8269bd0a4e5172a6c833424a876a2e4de52c34
LuisKlopp <fbgus333@gmail.com>
2024-04-12 14:26:00 +0900

    Style: 메인 컨테이너 padding 추가

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div>
       <ProfileBanner />
-      <div className="lg:mx-[10px] lgx:mx-[100px]">
+      <div className="pb-12 lg:mx-[10px] lgx:mx-[100px]">
         <h1 className="mb-10 mt-[60px] pl-[calc((100%-400px)/2)] text-xl font-medium md:pl-[calc((100%-820px)/2)] lg:pl-[calc((100%-1240px)/2)]">
           Most Viewed
         </h1>
