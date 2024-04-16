2125d2412fe2b4c991c0c668ef103222b5dc2d1e
LuisKlopp <fbgus333@gmail.com>
2024-04-16 15:00:44 +0900

    Feat: 좋아요카운트 추가 함수 생성 및 cache 제거

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -62,6 +62,9 @@ const getPostData = async (
 ) => {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/posts/${params?.slug[0]}`,
+    {
+      cache: "no-store",
+    },
   );
   const data = response.json();
   return data;
@@ -76,7 +79,7 @@ export default async function PostPage({
     notFound();
   }
 
-  const { likes, views } =
+  const { likes, views, id } =
     await getPostData(params);
 
   return (
@@ -105,7 +108,7 @@ export default async function PostPage({
       <div className="tracking-tight text-blogAbsoluteBlack dark:text-gray03">
         <MDXContent code={post.body} />
       </div>
-      <LikeButton likes={likes} />
+      <LikeButton likes={likes} postId={id} />
       <hr className="my-[10px] border border-gray06" />
       <span className="mt-[10px] text-base font-medium leading-8 text-blogAbsoluteBlack dark:text-white">
         3개의 댓글
--- components/like/like-button.tsx
+++ components/like/like-button.tsx
@@ -1,25 +1,62 @@
+"use client";
+
+import { cn } from "@/lib/utils";
 import LikeThumbsUp from "@/public/like-button.svg";
 import Image from "next/image";
+import { useState } from "react";
+
+interface LikeButtonProps {
+  likes: number;
+  postId: number;
+}
+
+const handleAddLikecount = async (
+  postId: number,
+) => {
+  await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/posts/like/${postId}`,
+    {
+      method: "POST",
+    },
+  );
+};
 
 export const LikeButton = ({
   likes,
-}: {
-  likes: number;
-}) => {
+  postId,
+}: LikeButtonProps) => {
+  const [likesCount, setLikesCount] =
+    useState(likes);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleAddLike = () => {
+    setLikesCount((prev) => prev + 1);
+    setIsLiked((prev) => !prev);
+    handleAddLikecount(postId);
+  };
+
   return (
     <div className="my-[50px] flex items-center gap-3">
-      <button className="flex h-10 w-[93px] items-center justify-center gap-2 rounded-lg bg-gray01 hover:bg-blogThickRed dark:bg-gray06 dark:hover:bg-blogThickRed">
+      <button
+        className={cn(
+          "flex h-10 w-[93px] items-center justify-center gap-2 rounded-lg bg-gray01 hover:bg-blogThickRed dark:bg-gray06 dark:hover:bg-blogThickRed",
+          {
+            "bg-blogThickRed dark:bg-blogThickRed":
+              isLiked,
+          },
+        )}
+        onClick={handleAddLike}
+      >
         <Image
           src={LikeThumbsUp}
           alt="thumbs-up"
-          className="my-0 h-4 w-4"
         />
         <span className="text-center font-medium leading-8 text-white">
           공감
         </span>
       </button>
       <span className=" text-base font-medium">
-        {likes}개
+        {likesCount}개
       </span>
     </div>
   );
