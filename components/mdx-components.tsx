e8e4bd5881a957efecfe6cd972373feea22dc100
LuisKlopp <fbgus333@gmail.com>
2024-05-01 14:36:31 +0900

    Feat: Mdx내에서 NextImage사용하기

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -2,13 +2,44 @@ import * as runtime from "react/jsx-runtime";
 import Image from "next/image";
 import { Callout } from "./callout";
 
+const StyledImage = ({
+  src,
+  alt,
+  height,
+  width,
+  ...props
+}: {
+  src: string;
+  alt: string;
+  height: string;
+  width: string;
+}) => {
+  return (
+    <div
+      style={{ height, width }}
+      className="relative overflow-hidden rounded-lg border-2 border-[#920028]"
+    >
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        sizes="100%"
+        className="m-0"
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default StyledImage;
+
 const useMDXComponent = (code: string) => {
   const fn = new Function(code);
   return fn({ ...runtime }).default;
 };
 
 const components = {
-  Image,
+  Image: StyledImage,
   Callout,
 };
 
