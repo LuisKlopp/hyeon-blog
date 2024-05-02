4b10a08a2b704131c855a8b11306937483c58b7a
LuisKlopp <fbgus333@gmail.com>
2024-05-02 15:28:15 +0900

    Feat: About-Me Profile 퍼블리싱

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -3,7 +3,7 @@ import AboutMeImage from "@/public/image/about-me-image.png";
 
 const AboutMe = () => {
   return (
-    <article className="container prose relative mx-auto max-w-3xl py-10 dark:prose-invert">
+    <article className="prose relative mx-auto max-w-3xl py-10 dark:prose-invert">
       <div>
         <h1 className="mb-3 text-xl font-medium leading-[43.2px]">
           류현
@@ -12,6 +12,30 @@ const AboutMe = () => {
           Front-End developer
         </span>
         <hr className="mb-0 mt-4 w-[60px] border border-black" />
+      </div>
+      <div className="mt-8 flex gap-7">
+        <Image
+          src={AboutMeImage}
+          alt="about-me-image"
+          className="m-0"
+        />
+        <div>
+          <h1 className="mb-2 text-xl font-thin">
+            빠르게,
+          </h1>
+          <h1 className="mb-2 text-xl font-thin">
+            꾸준히 배우고
+          </h1>
+          <div className="flex gap-2">
+            <h1 className="text-xl font-medium text-blogThickRed">
+              잘 적응하는 개발자
+            </h1>
+            <h1 className="text-xl font-medium">
+              류현입니다.
+            </h1>
+          </div>
+          <div></div>
+        </div>
       </div>
     </article>
   );
