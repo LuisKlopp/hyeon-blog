47b18d9697066bc197b44c7b2f79a75fde712e3d
LuisKlopp <fbgus333@gmail.com>
2024-05-02 14:23:36 +0900

    Feat: About-Me Top 퍼블리싱

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import AboutMeImage from "@/public/image/about-me-image.png";
+
 const AboutMe = () => {
-  return <div>AboutMe</div>;
+  return (
+    <article className="container prose relative mx-auto max-w-3xl py-10 dark:prose-invert">
+      <div>
+        <h1 className="mb-3 text-xl font-medium leading-[43.2px]">
+          류현
+        </h1>
+        <span className="text-base font-light leading-[21.48px] tracking-normal text-black">
+          Front-End developer
+        </span>
+        <hr className="mb-0 mt-4 w-[60px] border border-black" />
+      </div>
+    </article>
+  );
 };
 
 export default AboutMe;
