7bb4d816a958830f259fcf47f9f976a67f474669
LuisKlopp <fbgus333@gmail.com>
2024-05-15 13:03:45 +0900

    Feat: 수정 삭제 버튼 표출

--- components/comments/comment-item.tsx
+++ components/comments/comment-item.tsx
@@ -23,12 +23,19 @@ export const CommentItem = ({
   return (
     <div className="flex flex-col">
       <div className="mb-1 flex h-8 w-full items-center justify-between">
-        <span className="text-xs font-medium">
-          {nickname}
-        </span>
-        <span className="text-xs font-light text-gray02">
-          {date}
-        </span>
+        <div>
+          <span className="text-xs font-medium">
+            {nickname}{" "}
+          </span>
+          <span className="text-xs font-light text-gray04 dark:text-gray02">
+            &middot; {date}
+          </span>
+        </div>
+        <div className="text-xs text-gray02">
+          <span>수정</span>
+          <span> | </span>
+          <span>삭제</span>
+        </div>
       </div>
       <div className="flex">
         <span className="font-light leading-5">
